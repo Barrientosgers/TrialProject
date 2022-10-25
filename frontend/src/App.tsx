0959db434fefcca14a213e0e874cd7c3dc313828
Nathan Lee <nathanblee@gmail.com>
2022-10-25 11:42:40 -0700

    Added basic routing to main file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Welcome to the show</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<h1>Welcome</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
