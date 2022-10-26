325cc360d5ee7741485164d048637bac72f9da52
Nathan Lee <nathanblee@gmail.com>
2022-10-26 14:07:45 -0700

    Very basic '404 / not found' page

---
+++ frontend/src/shared/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import classes from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={classes["not-found-container"]}>
+      <h1>Page not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
