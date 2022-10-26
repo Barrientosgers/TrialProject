b9dfeaee0184f841401becbe74cfcb7cb773a969
Nathan Lee <nathanblee@gmail.com>
2022-10-26 16:02:55 -0700

    Added app updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,42 @@
+import React, { useState } from "react";
+
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import HomePage from "./home/pages/HomePage";
 import ListingPage from "./listings/pages/ListingPage";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  //const navigate = useNavigate();
+
+  const login = () => {
+    setIsLoggedIn(true);
+    //navigate("/");
+    console.log(isLoggedIn);
+  };
+
   return (
     <Router>
-      <MainNavigation />
+      <MainNavigation isLoggedIn={isLoggedIn} />
       <main>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/listing/:lid" element={<ListingPage />} />
-          <Route path="/login" element={<h1>Login</h1>} />
+          <Route
+            path="/login"
+            element={
+              <React.Fragment>
+                <h1>Login</h1>
+                <button onClick={login}>Login</button>
+              </React.Fragment>
+            }
+          />
         </Routes>
       </main>
     </Router>
