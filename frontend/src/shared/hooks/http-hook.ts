29f7a0e84a579afb562dc603cffbd776546820ed
Nathan Lee <nathanblee@gmail.com>
2022-11-08 14:57:43 -0800

    new http updated hook to typescript

---
+++ frontend/src/shared/hooks/http-hook.ts
@@ -0,0 +1,61 @@
+import { useCallback, useRef, useState, useEffect } from "react";
+
+export const useHttpClient = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const activeHttpRequests = useRef<AbortController[]>([]);
+
+  const sendRequest = useCallback(
+    async (url: string, method = "GET", body: any = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
+
+      try {
+        const response = await fetch(url, {
+          method,
+          body,
+          headers,
+          signal: httpAbortCtrl.signal,
+        });
+
+        const responseData = await response.json();
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (reqCtrl) => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        setIsLoading(false);
+
+        return responseData;
+      } catch (err: any) {
+        setError(err.message);
+        setIsLoading(false);
+
+        throw err;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((abortCtrl) => abortCtrl.abort());
+    };
+  }, []);
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+    clearError,
+  };
+};
